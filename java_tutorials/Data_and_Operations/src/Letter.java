4d3d94f06505a31ef95d5c663e14bb3aebe3072f
Dongwon Kang <dw3624@gmail.com>
2022-12-04 15:30:47 +0900

    first commit

---
+++ java_tutorials/Data_and_Operations/src/Letter.java
@@ -0,0 +1,13 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		
+		String name = "Tom";
+		System.out.println("Hello, "+name+" ... "+name+" ... boy ... bye");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+
+}
